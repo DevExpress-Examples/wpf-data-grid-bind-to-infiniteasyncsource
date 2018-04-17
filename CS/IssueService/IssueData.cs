648ec1995fe73f463d15d850e3d67afb63f2c7b6
Eugeniy Lapshov (DevExpress) <lapshov.eugeniy@corp.devexpress.com>
2018-04-17 15:16:26 +0300

    add example content

---
+++ CS/IssueService/IssueData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InfiniteAsyncSourceSample {
+    public class IssueData {
+        public IssueData(string subject, string user, DateTime created, int votes, Priority priority) {
+            Subject = subject;
+            User = user;
+            Created = created;
+            Votes = votes;
+            Priority = priority;
+        }
+        public string Subject { get; private set; }
+        public string User { get; private set; }
+        public DateTime Created { get; private set; }
+        public int Votes { get; private set; }
+        public Priority Priority { get; private set; }
+    }
+    public enum Priority { Low, BelowNormal, Normal, AboveNormal, High }
+}
---
+++ CS/IssueService/IssuesSummaries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InfiniteAsyncSourceSample {
+    public class IssuesSummaries {
+        public IssuesSummaries(int count, DateTime? lastCreated) {
+            Count = count;
+            LastCreated = lastCreated;
+        }
+
+        public int Count { get; private set; }
+        public DateTime? LastCreated { get; private set; }
+    }
+}
